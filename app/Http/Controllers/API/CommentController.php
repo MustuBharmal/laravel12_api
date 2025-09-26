caab3fec9b655f1cc7af15bccdba872655af57b6
MustuBharmal <mustafabharmal10@gmail.com>
2025-09-26 18:19:59 +0530

    comments

--- app/Http/Controllers/API/CommentController.php
+++ app/Http/Controllers/API/CommentController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CommentController extends Controller
 {
@@ -13,6 +15,13 @@ class CommentController extends Controller
     public function index()
     {
         //
+        $comments = Comment::get();
+
+        return response()->json([
+            'status' => 'success',
+            'count' => $comments->count(),
+            'data' => $comments,
+        ], 200);
     }
 
     /**
@@ -21,6 +30,28 @@ class CommentController extends Controller
     public function store(Request $request)
     {
         //
+        $validate = Validator::make($request->all(), [
+            'post_id' => 'required|integer|exists:blog_posts,id',
+            'content' => 'required|string|max:1000',
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json([
+                'status' => 'fail',
+                'errors' => $validate->errors(),
+            ], 400);
+        }
+
+        $data['post_id'] = $request->post_id;
+        $data['user_id'] = auth()->id();
+        $data['content'] = $request->content;
+
+        Comment::create($data);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Comment created and waiting for the admin approval',
+        ], 201);
     }
 
     /**
@@ -29,6 +60,43 @@ class CommentController extends Controller
     public function show(string $id)
     {
         //
+        $comment = Comment::where('post_id', $id)->where('status', 'approved')->get();
+        if ($comment) {
+            return response()->json([
+                'status' => 'success',
+                'data' => $comment,
+                'count' => $comment->count(),
+            ], 200);
+        }
+        return response()->json([
+            'status' => 'fail',
+            'message' => 'No comment found for this post',
+        ], 404);
+    }
+
+    // change status
+    public function changeStatus(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'comment_id' => 'required|exists:comments,id',
+            'status' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => $validator->errors(),
+            ], 400);
+        }
+
+        $comment = Comment::find($request->comment_id);
+        $comment->status = $request->status;
+        $comment->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Comment status changed successfully',
+        ], 201);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\API\AuthController;
 
 use App\Http\Controllers\API\BlogCategoryController;
 use App\Http\Controllers\API\BlogPostController;
+use App\Http\Controllers\API\CommentController;
 use App\Http\Controllers\API\LikeController;
 use Illuminate\Support\Facades\Route;
 
@@ -36,9 +37,18 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 
     Route::post('/blog-posts-image/{post}', [BlogPostController::class, 'blogPostImage'])->name('blog-posts-image')->middleware(['role:admin,author']);
 
+    // Like API routes
     Route::post('/post/react', [LikeController::class, 'react'])->name('react');
+
+    // Comment API routes
+    Route::apiResource('comments', CommentController::class);
+
+    Route::get('comments', [CommentController::class, 'index'])->name('index')->middleware(['role:admin']);
+
+    Route::post('/comments/changestatus', [CommentController::class, 'changeStatus'])->name('changeStatus')->middleware(['role:admin']);
 });
 
 Route::get('/blog-categories', [BlogCategoryController::class, 'index']);
 Route::get('/blog-posts', [BlogPostController::class, 'index']);
 Route::get('/post/reactions/{post}', [LikeController::class, 'reactions'])->name('reactions');
+Route::get('/comments/{comment}', [CommentController::class, 'show'])->name('show');
